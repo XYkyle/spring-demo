7135e054e2ea30985176391d37e8bf1686de4634
XYkyle <910706374@qq.com>
2021-12-01 19:19:57 +0800

    [提交]

---
+++ src/main/java/com/gxl/springdemo/SpringDemoApplication.java
@@ -0,0 +1,51 @@
+package com.gxl.springdemo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@MapperScan("com.gxl.springdemo.mapper")
+@EnableSwagger2
+public class SpringDemoApplication {
+
+    public static void main(String[] args) {
+        try{
+            SpringApplication.run(SpringDemoApplication.class, args);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.gxl.springdemo.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * 创建该API的基本信息（这些基本信息会展现在文档页面中）
+     * 访问地址：http://项目实际地址/swagger-ui.html
+     * @return
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("springboot-demo项目")
+                .description("测试项目")
+                .version("1.0")
+                .build();
+    }
+
+}
